8800d69938850b7bd99dec262ec56c48c09af628
Skiawm91 <guoy2674@outlook.com>
2024-11-21 21:11:26 +0800

    convert json to js

--- config.example.js
+++ config.example.js
@@ -1,7 +1,8 @@
-{
+const config = {
     "token": "YOUR_BOT_TOKEN",
     "clientId": "YOUR_CLIENT_ID",
     "errorChannelId": "YOUR_ERROR_LOG_CHANNEL_ID",
     "ngmcApiKey": "YOUR_NETHERGAMES_API_KEY",
     "ownerId": "YOUR_USER_ID"
 }
+module.exports = config;
